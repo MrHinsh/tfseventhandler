afbb09d574bd5bca1b3e69eff238098931e799ee
hinshelmw_cp <SND\hinshelmw_cp>
2007-04-30 15:01:12 +0000

    First Migration of Source Code: Alfa 1.0

---
+++ TFSEventHandler/RDdotNet.TeamFoundation.TestApp/Form1.vb
@@ -0,0 +1,28 @@
+Imports System.ServiceModel
+Imports System.ServiceModel.Description
+Imports System.Collections.Generic
+Imports MerrillLynch.TeamFoundation
+Imports MerrillLynch.TeamFoundation.Config
+Imports MerrillLynch.TeamFoundation.Client
+
+
+Public Class Form1
+
+
+#Region " Form1 "
+
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+
+
+    End Sub
+
+#End Region
+
+
+End Class
---
+++ TFSEventHandler/RDdotNet.TeamFoundation/Services/EventAdminService.vb
@@ -0,0 +1,328 @@
+Imports System.ServiceModel
+Imports System.Runtime.Serialization
+Imports System.Collections.ObjectModel
+Imports merrilllynch.TeamFoundation
+Imports merrilllynch.TeamFoundation.Config
+Imports microsoft.TeamFoundation
+Imports microsoft.TeamFoundation.Client
+Imports microsoft.TeamFoundation.Server
+
+<ServiceBehavior(InstanceContextMode:=InstanceContextMode.Single)> _
+Public Class EventAdminService
+    Implements MerrillLynch.TeamFoundation.ITeamServerAdmin
+    Implements MerrillLynch.TeamFoundation.ISubscriptionAdmin
+    Implements MerrillLynch.TeamFoundation.INotificationAdmin
+    Implements MerrillLynch.TeamFoundation.IEventHandlerAdmin
+
+    Implements IDisposable
+
+    Public ReadOnly Property ServiceSettings() As Config.ServiceItemElement
+        Get
+            Return Config.SettingsSection.Instance.Services.Item(Me.GetType.Name)
+        End Get
+    End Property
+
+    Public ReadOnly Property OperationContext() As OperationContext
+        Get
+            Return OperationContext.Current
+        End Get
+    End Property
+
+#Region " Team Server Bits "
+
+    Public Function GetTeamServer(ByVal TeamServerName As String) As TeamFoundationServer
+        Dim tfs As TeamFoundationServer = Nothing
+        Try
+            Dim ui As ICredentialsProvider = New UICredentialsProvider
+
+            Dim account As Net.NetworkCredential = New Net.NetworkCredential("srvteamsetup", "1Nst4ll4t10n", "EMEA")
+            tfs = New TeamFoundationServer(TeamServerName, account)
+        Catch ex As System.ServiceModel.FaultException
+            Throw ex
+        End Try
+        If tfs Is Nothing Then
+            Throw New System.ServiceModel.FaultException("Team Server not found")
+        End If
+        Return tfs
+    End Function
+
+    Public Function GetTeamServer(ByVal TeamServerUri As Uri) As Microsoft.TeamFoundation.Client.TeamFoundationServer
+        Dim serverName As String = Nothing
+        Try
+            serverName = RegisteredServers.GetServerForUri(TeamServerUri)
+        Catch ex As System.ServiceModel.FaultException
+            Throw ex
+        End Try
+        If serverName Is Nothing Then
+            Throw New System.ServiceModel.FaultException("Team Server not found")
+        End If
+        Return GetTeamServer(serverName)
+    End Function
+
+    Public Function GetServerSubs(ByVal TeamServerName As String) As Server.Subscription()
+        Try
+            Dim tfs As TeamFoundationServer = Me.GetTeamServer(TeamServerName)
+            Dim EventService As IEventService = CType(tfs.GetService(GetType(IEventService)), IEventService)
+            Return EventService.EventSubscriptions("EMEA\srvteamsetup", "EventAdminService")
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "GetServerSubs for TFS server unsucessfull")
+            Throw ex
+        End Try
+    End Function
+
+    Public Function GetServerSubscriptions(ByVal TeamServerName As String) As Collection(Of Subscription)
+        Try
+            Dim Subscriptions As New Collection(Of Subscription)
+            Dim ServerSubs() As Server.Subscription = GetServerSubs(TeamServerName)
+            For Each serverSub As Server.Subscription In ServerSubs
+                Subscriptions.Add(New Subscription(serverSub))
+            Next
+            Return Subscriptions
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "GetSubscriptions for TFS server unsucessfull")
+            Throw ex
+        End Try
+    End Function
+
+
+#End Region
+
+#Region " ITeamServerAdmin "
+
+    Private _TeamServerAdminCallback As ITeamServerAdminCallback
+
+    Public ReadOnly Property TeamServerAdminCallback() As ITeamServerAdminCallback
+        Get
+            If _TeamServerAdminCallback Is Nothing Then
+                _TeamServerAdminCallback = OperationContext.GetCallbackChannel(Of ITeamServerAdminCallback)()
+            End If
+            Return _TeamServerAdminCallback
+        End Get
+    End Property
+
+    Public Function ServceUrl() As System.Uri Implements ITeamServerAdmin.ServceUrl
+        Return OperationContext.EndpointDispatcher.EndpointAddress.Uri
+    End Function
+
+    Public Sub AddServer(ByVal TeamServerName As String, ByVal TeamServerUri As String) Implements ITeamServerAdmin.AddServer
+        Try
+            If RegisteredServers.GetUriForServer(TeamServerName) = Nothing Then
+                RegisteredServers.AddServer(TeamServerName, TeamServerUri)
+            End If
+            TeamServerAdminCallback.Updated(GetServers)
+            If Me.ServiceSettings.Debug.Verbose Then My.Application.Log.WriteEntry("Team Server Connected:" & TeamServerName)
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "Connection to TFS server unsucessfull")
+        End Try
+    End Sub
+
+    Public Sub RemoveServer(ByVal TeamServerName As String) Implements ITeamServerAdmin.RemoveServer
+        Try
+            RegisteredServers.RemoveServer(TeamServerName)
+            TeamServerAdminCallback.Updated(GetServers)
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "disconnection to TFS server unsucessfull")
+            Throw ex
+        End Try
+    End Sub
+
+    Public Function GetServers() As String() Implements ITeamServerAdmin.GetServers
+        Return RegisteredServers.GetServerNames
+    End Function
+
+#End Region
+
+#Region " ISubscriptionAdmin"
+
+    Private _SubscriptionAdminCallback As ISubscriptionAdminCallback
+
+    Public ReadOnly Property SubscriptionAdminCallback() As ISubscriptionAdminCallback
+        Get
+            If _SubscriptionAdminCallback Is Nothing Then
+                _SubscriptionAdminCallback = OperationContext.GetCallbackChannel(Of ISubscriptionAdminCallback)()
+            End If
+            Return _SubscriptionAdminCallback
+        End Get
+    End Property
+
+    Public Sub AddSubscriptions(ByVal ServiceUrl As String, ByVal EventType As EventTypes) Implements ISubscriptionAdmin.AddSubscriptions
+        Try
+            For Each TeamServerName As String In Me.GetServers()
+                Dim tfs As TeamFoundationServer = Me.GetTeamServer(TeamServerName)
+                Dim EventService As IEventService = CType(tfs.GetService(GetType(IEventService)), IEventService)
+                Dim delivery As DeliveryPreference = New DeliveryPreference()
+                delivery.Type = DeliveryType.Soap
+                delivery.Schedule = DeliverySchedule.Immediate
+                delivery.Address = ServiceUrl
+                Dim subId As Integer = EventService.SubscribeEvent("EMEA\srvteamsetup", EventType.ToString, "", delivery, "EventAdminService")
+                If Me.ServiceSettings.Debug.Verbose Then My.Application.Log.WriteEntry("Event Subscribed:" & TeamServerName)
+                SubscriptionAdminCallback.Updated(GetSubscriptions)
+            Next
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "AddSubscription to TFS server unsucessfull")
+            Throw ex
+        End Try
+    End Sub
+
+    Public Sub RemoveSubscriptions(ByVal ServiceUrl As String) Implements ISubscriptionAdmin.RemoveSubscriptions
+        Try
+            For Each TeamServerName As String In Me.GetServers()
+                Dim tfs As TeamFoundationServer = Me.GetTeamServer(TeamServerName)
+                Dim EventService As IEventService = CType(tfs.GetService(GetType(IEventService)), IEventService)
+                For Each SubScription As Subscription In GetServerSubscriptions(TeamServerName)
+                    If SubScription.Address = ServiceUrl Then
+                        EventService.UnsubscribeEvent(SubScription.ID)
+                        SubscriptionAdminCallback.Updated(GetSubscriptions)
+                    End If
+                Next
+            Next
+        Catch ex As System.ServiceModel.FaultException
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "RemoveSubscription for TFS server unsucessfull")
+            Throw ex
+        End Try
+    End Sub
+
+    Public Function GetSubscriptions() As System.Collections.ObjectModel.Collection(Of Subscription) Implements ISubscriptionAdmin.GetSubscriptions
+        Dim Subscriptions As New Collection(Of Subscription)
+        For Each TeamServerName As String In Me.GetServers()
+            Dim ServerSubs() As Server.Subscription = GetServerSubs(TeamServerName)
+            For Each serverSub As Server.Subscription In ServerSubs
+                Subscriptions.Add(New Subscription(serverSub))
+            Next
+        Next
+        Return Subscriptions
+    End Function
+
+#End Region
+
+#Region " IDisposable "
+
+    Private disposedValue As Boolean = False        ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                ' TODO: free managed resources when explicitly called
+
+            End If
+            ' TODO: free shared unmanaged resources
+        End If
+        Me.disposedValue = True
+    End Sub
+
+#Region " IDisposable Support "
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+#End Region
+
+#Region " INotificationAdmin "
+
+    Public Function GetEventType() As EventTypes Implements INotificationAdmin.GetEventType
+        Return EventTypes.Unknown
+    End Function
+
+    Public Function GetLocal() As String Implements INotificationAdmin.GetLocal
+        'Return Me.OperationContext.EndpointDispatcher.EndpointAddress.Uri.LocalPath
+        'Return System.Reflection.Assembly.GetExecutingAssembly.Location
+        ' Dim x As Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly
+
+    End Function
+
+#End Region
+
+#Region " IEventHandlerAdmin "
+
+#Region " Bits "
+
+    Public Enum XmlFiles
+        Manifest
+    End Enum
+
+    Private Function GetObject(Of T As {New})(ByVal XmlFile As XmlFiles) As T
+        Dim XmlFileLocation As String = SettingsSection.Instance.Repository.LocalPath
+        XmlFileLocation = System.IO.Path.Combine(XmlFileLocation, XmlFile.ToString & ".xml")
+        Dim x As New CustomXmlSerializer()
+        SyncLock x
+            GetObject = New T
+            If System.IO.File.Exists(XmlFileLocation) Then
+                GetObject = CType(x.ReadXml(XmlFileLocation, GetObject), T)
+            Else
+                x.WriteFile(GetObject, XmlFileLocation, True)
+            End If
+        End SyncLock
+        Return GetObject
+    End Function
+
+    Private Sub SetObject(Of T)(ByVal XmlFile As XmlFiles, ByVal target As T)
+        Dim XmlFileLocation As String = SettingsSection.Instance.Repository.LocalPath
+        XmlFileLocation = System.IO.Path.Combine(XmlFileLocation, XmlFile.ToString & ".xml")
+        Dim x As New CustomXmlSerializer()
+        SyncLock x
+            x.WriteFile(target, XmlFileLocation, True)
+        End SyncLock
+    End Sub
+
+#End Region
+
+    Private _EventHandlerAdminCallback As IEventHandlerAdminCallback
+
+    Public ReadOnly Property EventHandlerAdminCallback() As IEventHandlerAdminCallback
+        Get
+            If _EventHandlerAdminCallback Is Nothing Then
+                _EventHandlerAdminCallback = OperationContext.GetCallbackChannel(Of IEventHandlerAdminCallback)()
+            End If
+            Return _EventHandlerAdminCallback
+        End Get
+    End Property
+
+    Public Sub AddAssembly(ByVal AssemblyItem As AssemblyItem) Implements IEventHandlerAdmin.AddAssembly
+        If ValidateAssembly(AssemblyItem) Then
+            Dim AssemblyManaifest As AssemblyManaifest = Nothing
+            AssemblyManaifest = GetObject(Of AssemblyManaifest)(XmlFiles.Manifest)
+            AssemblyManaifest.Assemblys.Add(AssemblyItem)
+            AssemblyManaifest.Version = AssemblyManaifest.Version + 1
+            SetObject(Of AssemblyManaifest)(XmlFiles.Manifest, AssemblyManaifest)
+            EventHandlerAdminCallback.Updated(AssemblyManaifest)
+        End If
+    End Sub
+
+    Public Function GetAssemblys() As AssemblyManaifest Implements IEventHandlerAdmin.GetAssemblys
+        Return GetObject(Of AssemblyManaifest)(XmlFiles.Manifest)
+    End Function
+
+    Public Sub RemoveAssembly(ByVal ID As Integer) Implements IEventHandlerAdmin.RemoveAssembly
+
+    End Sub
+
+    Public Sub AddAssemblyDirect(ByVal AssemblyBytes As Byte()) Implements IEventHandlerAdmin.AddAssemblyDirect
+        AddAssembly(GetAssemblyItem(AssemblyBytes))
+    End Sub
+
+    Public Function GetAssemblyItem(ByVal AssemblyBytes As Byte()) As AssemblyItem Implements IEventHandlerAdmin.GetAssemblyItem
+        Try
+            '------------------
+            Dim AssemblyItem As AssemblyItem = AssemblyHelper.GetAssemblyItem(Config.SettingsSection.Instance.Repository.LocalPath, AssemblyBytes)
+            Return New AssemblyItem
+        Catch ex As Exception
+            Throw New System.ServiceModel.FaultException(Of Exception)(ex, "An error occurerd. That Assembly may not be of the correct type.")
+        End Try
+    End Function
+
+    Public Function ValidateAssembly(ByVal AssemblyItem As AssemblyItem) As Boolean Implements IEventHandlerAdmin.ValidateAssembly
+        If Not AssemblyItem.EventHandlers.Count > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+#End Region
+
+End Class
