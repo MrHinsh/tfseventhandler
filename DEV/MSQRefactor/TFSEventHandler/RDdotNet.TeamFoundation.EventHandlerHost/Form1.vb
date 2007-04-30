2c0910c015db3ea02a497da70953631dfefd7938
hinshelmw_cp <martin@hinshelwood.com>
2007-04-30 22:26:01 +0000

    Creating test host for Event Handler service

---
+++ DEV/MSQRefactor/TFSEventHandler/RDdotNet.TeamFoundation.EventHandlerHost/Form1.vb
@@ -0,0 +1,15 @@
+Public Class Form1
+
+    Private Host As System.ServiceModel.ServiceHost
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Host = New System.ServiceModel.ServiceHost(GetType(sometype))
+        Host.Open()
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Host.Close()
+    End Sub
+
+
+End Class
