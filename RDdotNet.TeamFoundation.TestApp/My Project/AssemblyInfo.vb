09b0ade44f95cd1fbfae4a45f3912801ddd3829a
hinshelmw_cp <martin@hinshelwood.com>
2007-04-30 22:06:32 +0000

    Rename of old Solution to Core

---
+++ RDdotNet.TeamFoundation.TestApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RDdotNet.TeamFoundation.TestApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Merrill Lynch")> 
+<Assembly: AssemblyProduct("RDdotNet.TeamFoundation.TestApp")> 
+<Assembly: AssemblyCopyright("Copyright © Merrill Lynch 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("689f80f6-ad2c-4c2e-b103-83464b2709a8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
