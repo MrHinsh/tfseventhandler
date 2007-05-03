a01d5f72ca1acabf5d5678b3ce5e4d66f5881ebd
hinshelmw_cp <martin@hinshelwood.com>
2007-05-03 16:00:02 +0000

    Move of all Queue services inhouse to Core

---
+++ DEV/MSQRefactor/Core/RDdotNet.TeamFoundation/Services/Contracts/ISubscriptions.vb
@@ -0,0 +1,30 @@
+Imports System.ServiceModel
+Imports System.Runtime.Serialization
+Imports System.Collections.ObjectModel
+Imports RDdotNet.TeamFoundation.Events
+
+Namespace Services.Contracts
+
+
+
+    ''' <summary>
+    ''' This is the seervice contract for integrating with the Team Foundation Server notification events.
+    ''' </summary>
+    ''' <remarks></remarks>
+    <ServiceContract(CallbackContract:=GetType(ISubscriptionsCallback), Namespace:="http://schemas.ml.com/TeamFoundation/2005/06/Services/SubscriptionAdmin")> _
+    Public Interface ISubscriptions
+
+        <OperationContract(IsOneWay:=True)> _
+        Sub AddSubscriptions(ByVal ServiceUrl As String, ByVal EventType As EventTypes)
+
+        <OperationContract(IsOneWay:=True)> _
+        Sub RemoveSubscriptions(ByVal ServiceUrl As String)
+
+        <OperationContract(IsOneWay:=False)> _
+        Function GetSubscriptions() As Collection(Of DataContracts.Subscription)
+
+
+    End Interface
+
+End Namespace
+
