2812dd76a1f0db85f4b6c1a151f699d08b0948af
hinshelmw_cp <SND\hinshelmw_cp>
2007-05-08 11:43:53 +0000

    Refactor: this is just rediculus

---
+++ DEV/MSQRefactor/TFSEventQueuer/RDdotNet.TeamFoundation.Queuer/Services/WorkItemChangedService.vb
@@ -0,0 +1,29 @@
+Imports System.ServiceModel
+Imports System.Runtime.Serialization
+Imports System.Collections.ObjectModel
+Imports RDdotNet.TeamFoundation
+Imports RDdotNet.TeamFoundation.Config
+
+Namespace Services
+
+
+
+    <ServiceBehavior(InstanceContextMode:=InstanceContextMode.Single)> _
+    Public Class WorkItemChangedService
+        Inherits AEventService(Of WorkItemChangedEvent)
+
+        Public Overrides Sub OnStart()
+
+        End Sub
+
+        Public Overrides Sub OnEnd()
+
+        End Sub
+
+        Public Overrides Sub OnEventRecieved(ByVal sender As Microsoft.TeamFoundation.Client.TeamFoundationServer, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent))
+
+        End Sub
+
+    End Class
+
+End Namespace
