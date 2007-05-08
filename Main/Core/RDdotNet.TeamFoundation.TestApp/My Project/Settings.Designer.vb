e3e5f634c93f0f1bedea148e118078428230f14f
hinshelmw_cp <SND\hinshelmw_cp>
2007-05-08 11:57:58 +0000

    Refactor: Done

--- Main/Core/RDdotNet.TeamFoundation.TestApp/My Project/Settings.Designer.vb
+++ Main/Core/RDdotNet.TeamFoundation.TestApp/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.RDdotNet.TeamFoundation.TestApp.My.MySettings
+        Friend ReadOnly Property Settings() As Global.RDdotNet.TeamFoundation.EventHandler.Explorer.My.MySettings
             Get
-                Return Global.RDdotNet.TeamFoundation.TestApp.My.MySettings.Default
+                Return Global.RDdotNet.TeamFoundation.EventHandler.Explorer.My.MySettings.Default
             End Get
         End Property
     End Module
