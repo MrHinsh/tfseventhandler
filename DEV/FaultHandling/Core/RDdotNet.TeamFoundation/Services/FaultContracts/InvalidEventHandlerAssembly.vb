560f4df3ae24e9475a7cc35d0e63dc33f7d79ec7
hinshelmw_cp <martin@hinshelwood.com>
2007-05-09 08:54:37 +0000

    Added fault handling branch

---
+++ DEV/FaultHandling/Core/RDdotNet.TeamFoundation/Services/FaultContracts/InvalidEventHandlerAssembly.vb
@@ -0,0 +1,23 @@
+Imports System.ServiceModel
+Imports System.ServiceModel.Description
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports RDdotNet.TeamFoundation
+Imports RDdotNet.TeamFoundation.Config
+Imports System.Runtime.Serialization
+
+Namespace Services.FaultContracts
+
+
+    <DataContract()> _
+    Public Class InvalidEventHandlerAssembly
+        Inherits System.ServiceModel.FaultException
+
+        Public Sub New()
+
+        End Sub
+
+    End Class
+
+
+End Namespace
