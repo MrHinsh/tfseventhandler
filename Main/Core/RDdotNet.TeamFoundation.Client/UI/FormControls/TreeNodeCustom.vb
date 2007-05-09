9615ff962efc996f8f498d40917b8d8600e473c9
hinshelmw_cp <martin@hinshelwood.com>
2007-05-09 09:45:52 +0000

    TreeNode refactor to simplify code

--- Main/Core/RDdotNet.TeamFoundation.Client/UI/FormControls/TreeNodeCustom.vb
+++ Main/Core/RDdotNet.TeamFoundation.Client/UI/FormControls/TreeNodeCustom.vb
@@ -14,7 +14,37 @@ Namespace UI.FormControls
     Public MustInherit Class TreeNodeCustom(Of T As {TreeNode})
         Inherits TreeNode
 
+        Private _EventHandler As TFSEventHandlerClient
         Private _Delay As Integer = 0
+        Private _NodeName As String = "[Enter Node Name]"
+        Private _Status As Status = Status.Normal
+
+        Public ReadOnly Property EventHandler() As TFSEventHandlerClient
+            Get
+                Return _EventHandler
+            End Get
+        End Property
+
+        Public ReadOnly Property Status() As Status
+            Get
+                Return _Status
+            End Get
+        End Property
+
+        Public Shadows ReadOnly Property ContextMenuStrip() As ContextMenuStrip
+            Get
+                If MyBase.ContextMenuStrip Is Nothing Then
+                    MyBase.ContextMenuStrip = New ContextMenuStrip
+                End If
+                Return MyBase.ContextMenuStrip
+            End Get
+        End Property
+
+        Public ReadOnly Property NodeName() As String
+            Get
+                Return _NodeName
+            End Get
+        End Property
 
         Public Property Delay() As Integer
             Get
@@ -25,11 +55,27 @@ Namespace UI.FormControls
             End Set
         End Property
 
+        Public Sub New(ByVal NodeName As String, ByVal EventHandler As TFSEventHandlerClient, Optional ByVal Delay As Integer = 0)
+            _EventHandler = EventHandler
+            _Delay = Delay
+            _NodeName = NodeName
+            Me.Name = NodeName
+        End Sub
+
+        Public Sub Refresh()
+            ' Initilise worker thread
+            System.Threading.ThreadPool.QueueUserWorkItem(AddressOf GenerateChildren)
+        End Sub
+
+        Protected MustOverride Sub GenerateChildren(ByVal state As Object)
+
+#Region " Thread Safe "
+
         Public Delegate Sub SimpleDelegate()
         Public Delegate Sub AddMessageDelegate(ByVal Message As String)
         Public Delegate Sub AddCatagoryErrorDelegate(ByVal Catagory As String, ByVal ex As Exception)
         Public Delegate Sub AddNodeDelegate(ByVal TreeNode As T)
-        Public Delegate Sub UpdateStatusDelegate(ByVal Header As String, ByVal Status As Status)
+        Public Delegate Sub UpdateStatusDelegate(ByVal Status As Status)
 
         Protected Sub ClearNodes()
             If Me.TreeView.InvokeRequired Then
@@ -39,7 +85,6 @@ Namespace UI.FormControls
                 MyBase.Nodes.Clear()
             End If
         End Sub
-
 
         Protected Overloads Sub ExpandAll()
             If Me.TreeView.InvokeRequired Then
@@ -77,19 +122,27 @@ Namespace UI.FormControls
             End If
         End Sub
 
-        Protected Sub UpdateStatus(ByVal Header As String, ByVal Status As Status)
+        Protected Sub ChangeStatus(ByVal Status As Status)
             If Me.TreeView.InvokeRequired Then
-                Me.TreeView.Invoke(New UpdateStatusDelegate(AddressOf UpdateStatus), Header, Status)
+                Me.TreeView.Invoke(New UpdateStatusDelegate(AddressOf ChangeStatus), Status)
                 System.Threading.Thread.Sleep(_Delay)
             Else
-                If Status = FormControls.Status.Normal Then
-                    Me.Text = Header
-                Else
-                    Me.Text = String.Format("{0} ({1})", Header, Status.ToString)
-                End If
+                _Status = Status
+                Select Case Status
+                    Case FormControls.Status.Normal
+                        Me.Text = Me.NodeName
+                    Case FormControls.Status.Working
+                        Me.Text = String.Format("{0} ({1}...)", Me.NodeName, Status.ToString)
+                    Case FormControls.Status.Faulted
+                        Me.Text = String.Format("{0} ({1})", Me.NodeName, Status.ToString)
+                End Select
             End If
         End Sub
 
+
+#End Region
+
+  
     End Class
 
 End Namespace
--- Main/Core/RDdotNet.TeamFoundation.Client/UI/FormControls/TreeNode_Subscriptions.vb
+++ Main/Core/RDdotNet.TeamFoundation.Client/UI/FormControls/TreeNode_Subscriptions.vb
@@ -9,53 +9,40 @@ Namespace UI.FormControls
     Friend Class TreeNode_Subscriptions
         Inherits TreeNodeCustom(Of TreeNode_Subscription)
 
-        Private _EventHandler As TFSEventHandlerClient
-        Private _ContextMenuStrip As New ContextMenuStrip
-
         Public Sub New(ByVal EventHandler As TFSEventHandlerClient, Optional ByVal Delay As Integer = 0)
-            Me.Text = "Subscriptions"
-            Me.Delay = Delay
+            MyBase.New("Subscriptions", EventHandler, Delay)
             '-----------------------
             ' Create Handler and attach Events
-            _EventHandler = EventHandler
-            AddHandler _EventHandler.SubscriptionsUpdated, AddressOf OnSubscriptionsUpdated
+            AddHandler EventHandler.SubscriptionsUpdated, AddressOf OnSubscriptionsUpdated
             '-----------------------
             ' Create Contect Menu as Add events
-            _ContextMenuStrip = New ContextMenuStrip
-            _ContextMenuStrip.Items.Add(New ToolStripButton("Add Subscription")) 'TODO:, Nothing, AddressOf AddTeamServer_Click))
-            Me.ContextMenuStrip = _ContextMenuStrip
+            ContextMenuStrip.Items.Add(New ToolStripButton("Add Subscription")) 'TODO:, Nothing, AddressOf AddTeamServer_Click))
             '-----------------------
             ' Initilise team server List
-            System.Threading.ThreadPool.QueueUserWorkItem(AddressOf GenerateChildren)
+            Refresh()
         End Sub
-
-        Friend ReadOnly Property EventHandler() As TFSEventHandlerClient
-            Get
-                Return _EventHandler
-            End Get
-        End Property
 
         Public Sub OnSubscriptionsUpdated(ByVal subscriptions As Collection(Of Subscription))
             GenerateChildren(subscriptions)
         End Sub
 
-        Public Sub GenerateChildren(ByVal State As Object)
-            Me.UpdateStatus("Subscriptions", Status.Working)
+        Protected Overrides Sub GenerateChildren(ByVal State As Object)
+            Me.ChangeStatus(Status.Working)
             Dim subscriptions As Collection(Of Subscription) = Nothing
             Try
-                subscriptions = _EventHandler.GetSubscriptions()
+                subscriptions = EventHandler.GetSubscriptions()
             Catch ex As Services.FaultContracts.TeamFoundationServerUnauthorizedException
                 AddError("TFS Denied", ex)
-                Me.UpdateStatus("Subscriptions", Status.Faulted)
+                Me.ChangeStatus(Status.Faulted)
             Catch ex As ServiceModel.FaultException
                 AddError("Error", ex)
-                Me.UpdateStatus("Subscriptions", Status.Faulted)
+                Me.ChangeStatus(Status.Faulted)
             Finally
                 GenerateChildren(subscriptions)
             End Try
         End Sub
 
-        Public Sub GenerateChildren(ByVal subscriptions As Collection(Of Subscription))
+        Public Overloads Sub GenerateChildren(ByVal subscriptions As Collection(Of Subscription))
             ClearNodes()
             If Not subscriptions Is Nothing Then
                 For Each s As Subscription In subscriptions
@@ -67,7 +54,7 @@ Namespace UI.FormControls
             End If
             ' Then make sure that all nodes are expanded
             Me.ExpandAll()
-            Me.UpdateStatus("Subscriptions", Status.Normal)
+            Me.ChangeStatus(Status.Normal)
         End Sub
 
     End Class
