f081de4b73e1d850eb02b7891422d210d1b99684
hinshelmw_cp <SND\hinshelmw_cp>
2007-06-08 10:54:56 +0000

    Refactor for global wrappers

--- DEV/hinshelmw/Core/RDdotNet.Clients/IService.vb
+++ DEV/hinshelmw/Core/RDdotNet.Clients/IService.vb
@@ -1,10 +1,5 @@
-Imports System.ServiceModel
 Imports System.Runtime.Serialization
 Imports System.Collections.ObjectModel
-Imports RDdotNet.TeamFoundation
-Imports RDdotNet.TeamFoundation.Services
-Imports RDdotNet.TeamFoundation.Config
-Imports RDdotNet.TeamFoundation.Events
 
 Namespace Clients
 
