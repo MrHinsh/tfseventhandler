9b368a7ffeb33d7f06ea5de12104f49734a059d7
hinshelmw_cp <SND\hinshelmw_cp>
2007-06-18 09:48:59 +0000

    Added prototype code to this implementation

---
+++ PROTOTYPE/RDdotNet.TeamFoundation.TestApp/Form1.vb
@@ -0,0 +1,209 @@
+Imports System.ServiceModel
+Imports System.ServiceModel.Description
+Imports System.Collections.Generic
+Imports RDdotNet.TeamFoundation
+Imports RDdotNet.TeamFoundation.Config
+
+
+Public Class Form1
+
+    Public WithEvents TeamServerManager As New TeamServerManager()
+    Public WithEvents ServiceHostManager As New ServiceHostManager()
+    Public WithEvents EventHandlerManager_WorkItemChanged As New EventHandlersManager(Of WorkItemChangedEvent)(EventTypes.WorkItemChangedEvent, False)
+    Public WithEvents EventHandlerManager_CheckinEvent As New EventHandlersManager(Of CheckinEvent)(EventTypes.CheckinEvent, False)
+
+#Region " Form1 "
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        ServiceHostManager.Dispose()
+        TeamServerManager.Dispose()
+        EventHandlerManager_WorkItemChanged.Dispose()
+        EventHandlerManager_CheckinEvent.Dispose()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        '----------------------------
+
+        '----------------------------
+
+    End Sub
+
+    Private Sub ButtonStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonStart.Click
+        TeamServerManager.Initilise()
+        ButtonEnd.Enabled = True
+        ButtonStart.Enabled = False
+    End Sub
+
+    Private Sub ButtonEnd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonEnd.Click
+        ServiceHostManager.Dispose()
+        TeamServerManager.Dispose()
+        EventHandlerManager_WorkItemChanged.Dispose()
+        EventHandlerManager_CheckinEvent.Dispose()
+        ButtonEnd.Enabled = False
+        ButtonStart.Enabled = True
+    End Sub
+
+#End Region
+
+#Region " TeamServerManager "
+
+    Private Delegate Sub TeamServerManager_StatusChangeDelegate(ByVal ManagedType As TeamServerItem, ByVal Status As Status, ByVal Items As Integer)
+    Private Delegate Sub TeamServerManager_ErrorDelegate(ByVal ManagedType As TeamServerItem, ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception)
+    Private Delegate Sub TeamServerManager_InitiliseCompleteDelegate()
+
+    Private Sub TeamServerManager_Error(ByVal ManagedType As TeamServerItem, ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception) Handles TeamServerManager.Error
+        If Me.InvokeRequired Then
+            Me.Invoke(New TeamServerManager_ErrorDelegate(AddressOf TeamServerManager_Error), ManagedType, Status, Items, e)
+        Else
+            Me.uxListBoxTeamServer.Items.Add("Error: " & e.ToString)
+        End If
+    End Sub
+
+    Private Sub TeamServerManager_StatusChange(ByVal TeamServer As TeamServerItem, ByVal Status As Status, ByVal Items As Integer) Handles TeamServerManager.StatusChange
+        If Me.InvokeRequired Then
+            Me.Invoke(New TeamServerManager_StatusChangeDelegate(AddressOf TeamServerManager_StatusChange), TeamServer, Status, Items)
+        Else
+            Me.uxListBoxTeamServer.Items.Add("StatusChange: " & Status.ToString)
+            Select Case Status
+                Case TeamFoundation.Status.Connected
+                    ' MsgBox("TeamServerManager_TeamServerCreated: " & TeamServer.Subject.Name)
+                Case TeamFoundation.Status.InitializationComplete
+                    ServiceHostManager.Initilise()
+            End Select
+        End If
+    End Sub
+
+#End Region
+
+#Region " ServiceHostManager "
+
+    Private Delegate Sub ServiceHostManager_StatusChangeDelegate(ByVal ManagedType As ServiceHostItem, ByVal Status As Status, ByVal Items As Integer)
+    Private Delegate Sub ServiceHostManager_ErrorDelegate(ByVal ManagedType As ServiceHostItem, ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception)
+    Private Delegate Sub ServiceHostManager_InitiliseCompleteDelegate()
+
+  
+
+    Private Sub ServiceHostManager_Error(ByVal ManagedType As ServiceHostItem, ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception) Handles ServiceHostManager.Error
+        If Me.InvokeRequired Then
+            Me.Invoke(New ServiceHostManager_ErrorDelegate(AddressOf ServiceHostManager_Error), ManagedType, Status, Items, e)
+        Else
+            Me.uxListBoxServiceHost.Items.Add("Error: " & Status.ToString & " " & e.ToString)
+        End If
+    End Sub
+
+    Private Sub ServiceHostManager_StatusChange(ByVal ManagedType As ServiceHostItem, ByVal Status As Status, ByVal Items As Integer) Handles ServiceHostManager.StatusChange
+        If Me.InvokeRequired Then
+            Me.Invoke(New ServiceHostManager_StatusChangeDelegate(AddressOf ServiceHostManager_StatusChange), ManagedType, Status, Items)
+        Else
+            Me.uxListBoxServiceHost.Items.Add("StatusChange: " & Status.ToString)
+            Select Case Status
+                Case TeamFoundation.Status.Closed
+                    'MsgBox("ServiceHostManager_ServiceHostClosed: " & ManagedType.EventType.ToString & ":" & ManagedType.BaseAddress.ToString)
+                    TeamServerManager.UnregisterEvent(ManagedType.EventType)
+                Case TeamFoundation.Status.Connected
+                    'MsgBox("ServiceHostManager_ServiceHostCreated: " & ManagedType.EventType.ToString & ":" & ManagedType.BaseAddress.ToString)
+                    TeamServerManager.RegisterEvent(ManagedType.EventType, ManagedType.BaseAddress)
+                Case TeamFoundation.Status.InitializationComplete
+                    EventHandlerManager_WorkItemChanged.Initilise()
+                    EventHandlerManager_CheckinEvent.Initilise()
+            End Select
+        End If
+    End Sub
+
+    Private Delegate Sub ServiceHostManager_WorkItemChangedEventDelegate(ByVal ServiceHost As ServiceHostItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent))
+
+    Private Sub ServiceHostManager_WorkItemChangedEvent(ByVal ServiceHost As ServiceHostItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent)) Handles ServiceHostManager.WorkItemChangedEvent
+        If Me.InvokeRequired Then
+            Me.Invoke(New ServiceHostManager_WorkItemChangedEventDelegate(AddressOf ServiceHostManager_WorkItemChangedEvent), ServiceHost, e)
+        Else
+            Me.uxListBoxServiceHost.Items.Add("Event Recieved: " & e.EventID.ToString)
+
+            e.Event.DisplayUrl = EndpointBase.ReformatServerURL(EventTypes.WorkItemChangedEvent, e.Event.DisplayUrl)
+
+            EventHandlerManager_WorkItemChanged.RunEventHandlers(ServiceHost, TeamServerManager.GetTeamServer(e.Identity), e)
+        End If
+    End Sub
+
+    Private Delegate Sub ServiceHostManager_CheckinEventDelegate(ByVal ServiceHost As ServiceHostItem, ByVal e As NotifyEventArgs(Of CheckinEvent))
+
+    Private Sub ServiceHostManager_CheckinEvent(ByVal ServiceHost As ServiceHostItem, ByVal e As NotifyEventArgs(Of CheckinEvent)) Handles ServiceHostManager.CheckinEvent
+        If Me.InvokeRequired Then
+            Me.Invoke(New ServiceHostManager_CheckinEventDelegate(AddressOf ServiceHostManager_CheckinEvent), ServiceHost, e)
+        Else
+            Me.uxListBoxServiceHost.Items.Add("Event Recieved: " & e.EventID.ToString)
+
+            ' e.Event.DisplayUrl = EndpointBase.ReformatServerURL(EventTypes.WorkItemChangedEvent, e.Event.DisplayUrl)
+
+            EventHandlerManager_CheckinEvent.RunEventHandlers(ServiceHost, TeamServerManager.GetTeamServer(e.Identity), e)
+        End If
+    End Sub
+
+#End Region
+
+#Region " EventHandlerManager_WorkItemChanged "
+
+    Private Delegate Sub WorkItemChangedManager_StatusChangeDelegate(ByVal ManagedType As EventHandlerItem(Of WorkItemChangedEvent), ByVal Status As Status, ByVal Items As Integer)
+    Private Delegate Sub WorkItemChangedManager_ErrorDelegate(ByVal ManagedType As EventHandlerItem(Of WorkItemChangedEvent), ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception)
+    Private Delegate Sub WorkItemChangedManager_InitiliseCompleteDelegate()
+
+
+    Private Sub WorkItemChangedManager_Error(ByVal ManagedType As EventHandlerItem(Of WorkItemChangedEvent), ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception) Handles EventHandlerManager_WorkItemChanged.Error
+        If Me.InvokeRequired Then
+            Me.Invoke(New WorkItemChangedManager_ErrorDelegate(AddressOf WorkItemChangedManager_Error), ManagedType, Status, Items, e)
+        Else
+            Me.uxListBoxWorkItemChanged.Items.Add("Error: " & e.ToString)
+        End If
+    End Sub
+
+    Private Sub WorkItemChangedManager_StatusChange(ByVal ManagedType As EventHandlerItem(Of WorkItemChangedEvent), ByVal Status As Status, ByVal Items As Integer) Handles EventHandlerManager_WorkItemChanged.StatusChange
+        If Me.InvokeRequired Then
+            Me.Invoke(New WorkItemChangedManager_StatusChangeDelegate(AddressOf WorkItemChangedManager_StatusChange), ManagedType, Status, Items)
+        Else
+            Me.uxListBoxWorkItemChanged.Items.Add("StatusChange: " & Status.ToString)
+        End If
+    End Sub
+
+#End Region
+
+
+#Region " EventHandlerManager_CheckinEvent "
+
+    Private Delegate Sub EventHandlerManager_CheckinEvent_StatusChangeDelegate(ByVal ManagedType As EventHandlerItem(Of CheckinEvent), ByVal Status As Status, ByVal Items As Integer)
+    Private Delegate Sub EventHandlerManager_CheckinEvent_ErrorDelegate(ByVal ManagedType As EventHandlerItem(Of CheckinEvent), ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception)
+
+
+    Private Sub EventHandlerManager_CheckinEvent_Error(ByVal ManagedType As EventHandlerItem(Of CheckinEvent), ByVal Status As Status, ByVal Items As Integer, ByVal e As System.Exception) Handles EventHandlerManager_CheckinEvent.Error
+        If Me.InvokeRequired Then
+            Me.Invoke(New EventHandlerManager_CheckinEvent_ErrorDelegate(AddressOf EventHandlerManager_CheckinEvent_Error), ManagedType, Status, Items, e)
+        Else
+            uxListBoxCheckIn.Items.Add("Error: " & e.ToString)
+        End If
+    End Sub
+
+    Private Sub EventHandlerManager_CheckinEvent_StatusChange(ByVal ManagedType As EventHandlerItem(Of CheckinEvent), ByVal Status As Status, ByVal Items As Integer) Handles EventHandlerManager_CheckinEvent.StatusChange
+        If Me.InvokeRequired Then
+            Me.Invoke(New EventHandlerManager_CheckinEvent_StatusChangeDelegate(AddressOf EventHandlerManager_CheckinEvent_StatusChange), ManagedType, Status, Items)
+        Else
+            uxListBoxCheckIn.Items.Add("StatusChange: " & Status.ToString)
+        End If
+    End Sub
+
+#End Region
+
+#Region " uxListBox "
+
+    Private Sub uxListBox_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles uxListBoxTeamServer.MouseClick, uxListBoxServiceHost.MouseClick, uxListBoxCheckIn.MouseClick
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            Dim x As ListBox = CType(sender, ListBox)
+            Me.uxContextMenuStripDetails.Show(e.X, e.Y)
+        End If
+    End Sub
+
+#End Region
+
+
+
+
+
+ 
+End Class
---
+++ PROTOTYPE/RDdotNet.TeamFoundation/Managers/TeamServerManager.vb
@@ -0,0 +1,107 @@
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports RDdotNet.TeamFoundation
+Imports RDdotNet.TeamFoundation.Config
+Imports Microsoft.TeamFoundation.Client
+Imports Microsoft.TeamFoundation
+
+Public Class TeamServerManager
+    Inherits AManager(Of TeamServerItem)
+
+    Public Sub New()
+        MyBase.New(False)
+    End Sub
+
+    Private _TeamServers As New Collection(Of TeamServerItem)
+
+    Public ReadOnly Property TeamServers() As Collection(Of TeamServerItem)
+        Get
+            Return _TeamServers
+        End Get
+    End Property
+
+    Public Overrides Sub ManagerBeginCustom(ByVal state As Object)
+        ' Connect to all team servers
+        For Each TeamServerItem As TeamServerItemElement In Settings.TeamServerItems
+            Dim TeamServer As TeamServerItem = New TeamServerItem(Nothing, TeamServerItem)
+            Dim Status As Status = Status.Connecting
+            Try
+                Me.OnStatusChange(TeamServer, Status, TeamServers.Count)
+                TeamServer.Subject = GetTeamServer(TeamServerItem)
+                Status = Status.Connected
+                TeamServers.Add(TeamServer)
+                Me.OnStatusChange(TeamServer, Status, TeamServers.Count)
+            Catch ex As Exception
+                Me.OnError(TeamServer, Status, TeamServers.Count, ex)
+            End Try
+        Next
+    End Sub
+
+    Public Overrides Sub ManagerEndCustom()
+        '--------------
+        ' remove all events
+        For Each EventItem As Config.EventItemElement In Settings.EventItems
+            UnregisterEvent(EventItem.EventType)
+        Next
+        '--------------
+        ' kill all team server commentsions
+        For Each TeamServerItem As TeamServerItem In TeamServers
+            Me.OnStatusChange(TeamServerItem, Status.Closing, TeamServers.Count)
+            TeamServerItem.Subject.Dispose()
+            Me.OnStatusChange(TeamServerItem, Status.Closed, TeamServers.Count)
+        Next
+        '--------------
+        TeamServers.Clear()
+        '--------------
+    End Sub
+
+    Private Function GetTeamServer(ByVal TeamServerItem As Config.TeamServerItemElement) As TeamFoundationServer
+        Try
+            If RegisteredServers.GetUriForServer(TeamServerItem.Name) = Nothing Then
+                RegisteredServers.AddServer(TeamServerItem.Name, TeamServerItem.Url.ToString)
+            End If
+            GetTeamServer = TeamFoundationServerFactory.GetServer(TeamServerItem.Name)
+        Catch ex As Exception
+            Me.OnError(Nothing, Status.None, TeamServers.Count, ex)
+            My.Application.Log.WriteException(ex, TraceEventType.Error, "Connection to TFS server unsucessfull")
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function GetTeamServer(ByVal EventIdentity As TFSIdentity) As TeamServerItem
+        For Each TeamServerItem As TeamServerItem In TeamServers
+            If EventIdentity.Url.ToLower.IndexOf(TeamServerItem.Subject.Name.ToLower) > -1 Then
+                Return TeamServerItem
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    Public Sub RegisterEvent(ByVal EventType As EventTypes, ByVal BaseAddress As System.Uri)
+        Dim SubscriptionID As Integer = 0
+        For Each TeamServerItem As TeamServerItem In TeamServers
+            Try
+                Dim ID As Integer = 0
+                ID = SubscriptionServices.SubscribeEvent(TeamServerItem.Subject, TeamServerItem.ItemElement.Subscriber, BaseAddress.ToString, Server.DeliveryType.Soap, Server.DeliverySchedule.Immediate, EventType)
+                TeamServerItem.CollectSubscriptionID(EventType, ID)
+            Catch ex As Exception
+                Me.OnError(TeamServerItem, Status.External, TeamServers.Count, ex)
+            End Try
+        Next
+    End Sub
+
+    Public Sub UnregisterEvent(ByVal EventType As EventTypes)
+        Dim SubscriptionID As Integer = 0
+        For Each TeamServerItem As TeamServerItem In TeamServers
+            Dim ID As Integer = TeamServerItem.GetSubscription(EventType)
+            If Not ID = 0 Then
+                Try
+                    SubscriptionServices.UnSubscribeEvent(TeamServerItem.Subject, TeamServerItem.GetSubscription(EventType))
+                Catch ex As Exception
+                    Me.OnError(TeamServerItem, Status.External, TeamServers.Count, ex)
+                End Try
+            End If
+        Next
+    End Sub
+
+End Class
