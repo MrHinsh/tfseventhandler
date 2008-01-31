0e31522cac851435eb42914ed69b749ea7922420
hinshelmw_cp <martin@hinshelwood.com>
2008-01-31 10:32:34 +0000

    New Event Handlers augmented and tested for load capability]

---
+++ RELEASE/v0.0.1 Prototype/Event Handlers/AssignedTo/ReAssignedHandler.vb
@@ -0,0 +1,66 @@
+Imports System.Xml
+Imports System.Xml.Xsl
+Imports System.Xml.XPath
+Imports System.io
+Imports System.Text
+Imports System.Net.Mail
+Imports System.Configuration
+Imports System.Reflection
+Imports Microsoft.TeamFoundation.Client
+Imports RDdotNet.TeamFoundation.Helpers
+Imports RDdotNet.TeamFoundation
+
+Public Class ReAssignedHandler
+    Implements IEventHandler(Of WorkItemChangedEvent)
+
+#Region " IEventHandler "
+
+    '' <summary>
+    '' Returns true if the event contains a new assignment to a user other than the assigner
+    '' </summary>
+    Public Function IsValid(ByVal EventHandlerItem As EventHandlerItem(Of WorkItemChangedEvent), ByVal ServiceHost As ServiceHostItem, ByVal TeamServer As TeamServerItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent)) As Boolean Implements IEventHandler(Of WorkItemChangedEvent).IsValid
+        If e.Event Is Nothing Then
+            Return False
+        End If
+        Dim NewAssignedName As String = Querys.GetAssignedToName(e.Event).NewValue
+        Dim OldAssignedName As String = Querys.GetAssignedToName(e.Event).OldValue
+        Dim ChangedByName As String = WorkItemEventQuerys.GetChangedByName(e.Event)
+        If String.IsNullOrEmpty(NewAssignedName) Or String.IsNullOrEmpty(OldAssignedName) Then
+            Return False
+        Else
+            Return Not NewAssignedName = OldAssignedName And Not NewAssignedName = ChangedByName
+        End If
+    End Function
+
+    Public Sub Run(ByVal EventHandlerItem As EventHandlerItem(Of WorkItemChangedEvent), ByVal ServiceHost As ServiceHostItem, ByVal TeamServer As TeamServerItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent)) Implements IEventHandler(Of WorkItemChangedEvent).Run
+        If TeamServer.ItemElement.LogEvents Then My.Application.Log.WriteEntry("ReAssignedHandler: Running ")
+        If Not IsValid(EventHandlerItem, ServiceHost, TeamServer, e) Then
+            Return
+        End If
+        Dim toName As String = Querys.GetAssignedToName(e.Event).OldValue
+        Dim toAddress As String = RDdotNet.ActiveDirectory.Querys.GetEmailAddress(toName)
+        Dim fromName As String = WorkItemEventQuerys.GetChangedByName(e.Event)
+        Dim fromAddress As String = RDdotNet.ActiveDirectory.Querys.GetEmailAddress(fromName)
+        If String.IsNullOrEmpty(toAddress) Then
+            'Logger.Log("Can't send email because no email address was found for " + toName)
+        Else
+            Dim [to] As New MailAddress(toAddress, toName)
+            Dim from As New MailAddress(fromAddress, fromName)
+            If TeamServer.ItemElement.TestMode Then
+                [to] = New Net.Mail.MailAddress(TeamServer.ItemElement.TestEmail)
+            End If
+            Dim Subject As String = "##PortfolioProject##:##WorkItemType## Re-Assigned - ##WorkItemID##: ##WorkItemTitle##"
+            Dim x As New Mail(EventHandlerItem, TeamServer, e)
+            x.SendMail(Mail.EmailTypes.ReAssigned, [to], from, Subject)
+        End If
+        If TeamServer.ItemElement.LogEvents Then My.Application.Log.WriteEntry("ReAssignedHandler: Complete ")
+    End Sub
+
+
+#End Region
+
+
+
+
+
+End Class
