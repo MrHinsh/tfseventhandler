f496c8d78ec44f06074e7a62ba39a8c637424ed2
hinshelmw_cp <martin@hinshelwood.com>
2008-11-28 11:50:18 +0000

    Created SAFE branch for release v1.1

---
+++ SAFE/v1.1/RDdotNet.TeamFoundation.EventHandlers.AssignedTo/ReAssignedHandler.vb
@@ -0,0 +1,71 @@
+Imports System.Xml
+Imports System.Xml.Xsl
+Imports System.Xml.XPath
+Imports System.io
+Imports System.Text
+Imports System.Net.Mail
+Imports System.Configuration
+Imports System.Reflection
+Imports Microsoft.TeamFoundation.Client
+Imports Hinshelwood.TeamFoundation.Helpers
+Imports Hinshelwood.TeamFoundation
+
+''' <summary>
+''' Send an email to a user when a work item that they are assigned is re assigend to someone else unless they are the one that made the change.
+''' </summary>
+''' <remarks></remarks>
+Public Class ReAssignedHandler
+    Implements IEventHandler(Of WorkItemChangedEvent)
+
+#Region " IEventHandler "
+
+    '' <summary>
+    '' Returns true if the event contains a new assignment to a user other than the assigner
+    '' </summary>
+    Public Function IsValid(ByVal EventHandlerItem As EventHandlerItem(Of WorkItemChangedEvent), ByVal ServiceHost As ServiceHostItem, ByVal TeamServer As TeamServerItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent)) As Boolean Implements IEventHandler(Of WorkItemChangedEvent).IsValid
+        If e.Event Is Nothing Then
+            Return False
+        End If
+        Dim NewAssignedName As String = Querys.GetAssignedToName(e.Event).NewValue
+        Dim OldAssignedName As String = Querys.GetAssignedToName(e.Event).OldValue
+        Dim ChangedByName As String = WorkItemEventQuerys.GetChangedByName(e.Event)
+        If String.IsNullOrEmpty(NewAssignedName) Or String.IsNullOrEmpty(OldAssignedName) Then
+            Return False
+        Else
+            Return Not NewAssignedName = OldAssignedName And Not OldAssignedName = ChangedByName
+        End If
+    End Function
+
+    Public Sub Run(ByVal EventHandlerItem As EventHandlerItem(Of WorkItemChangedEvent), ByVal ServiceHost As ServiceHostItem, ByVal TeamServer As TeamServerItem, ByVal e As NotifyEventArgs(Of WorkItemChangedEvent)) Implements IEventHandler(Of WorkItemChangedEvent).Run
+        If TeamServer.ItemElement.LogEvents Then My.Application.Log.WriteEntry("ReAssignedHandler: Running ")
+        If Not IsValid(EventHandlerItem, ServiceHost, TeamServer, e) Then
+            If TeamServer.ItemElement.LogEvents Then My.Application.Log.WriteEntry("ReAssignedHandler: Is not valid ", TraceEventType.Warning)
+            Return
+        End If
+        Dim toName As String = Querys.GetAssignedToName(e.Event).OldValue
+        Dim toAddress As String = Hinshelwood.ActiveDirectory.Querys.GetEmailAddress(toName)
+        Dim fromName As String = WorkItemEventQuerys.GetChangedByName(e.Event)
+        Dim fromAddress As String = Hinshelwood.ActiveDirectory.Querys.GetEmailAddress(fromName)
+        If String.IsNullOrEmpty(toAddress) Then
+            'Logger.Log("Can't send email because no email address was found for " + toName)
+        Else
+            Dim [to] As New MailAddress(toAddress, toName)
+            Dim from As New MailAddress(fromAddress, fromName)
+            If TeamServer.ItemElement.TestMode Then
+                [to] = New Net.Mail.MailAddress(TeamServer.ItemElement.TestEmail)
+            End If
+            Dim Subject As String = "##PortfolioProject##:##WorkItemType## Re-Assigned - ##WorkItemID##: ##WorkItemTitle##"
+            Dim x As New Mail(EventHandlerItem, TeamServer, e)
+            x.SendMail("ReAssigned", [to], from, Subject)
+        End If
+        If TeamServer.ItemElement.LogEvents Then My.Application.Log.WriteEntry("ReAssignedHandler: Complete ")
+    End Sub
+
+
+#End Region
+
+
+
+
+
+End Class
