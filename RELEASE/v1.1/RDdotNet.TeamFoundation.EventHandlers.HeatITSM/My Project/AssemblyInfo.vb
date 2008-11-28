329f70abc532af7b71ca7573aa7c5a604861dfa9
hinshelmw_cp <SND\hinshelmw_cp>
2008-11-28 11:49:08 +0000

    Created Release branch for v1.1

---
+++ RELEASE/v1.1/RDdotNet.TeamFoundation.EventHandlers.HeatITSM/My Project/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Heat ITSM Event Handler")> 
+
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a13bf88e-5a80-4948-948b-d34e0cb74def")> 
+
