3f446917083cd64d8344b0f92b4cd8cc44fe7e82
hinshelmw_cp <martin@hinshelwood.com>
2008-11-28 11:49:08 +0000

    Created Release branch for v1.1

---
+++ RDdotNet.TeamFoundation/My Project/AssemblyInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Team Foundation")> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("793802a2-595e-4580-9338-b09426fdb0d8")> 
+
