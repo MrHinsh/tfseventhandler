40c004607298ffeb2858c9bcad2aad968fc010d4
hinshelmw_cp <SND\hinshelmw_cp>
2008-12-02 14:28:44 +0000

    Create SAFE v1.3

---
+++ SAFE/v1.3/RDdotNet.TeamFoundation.WorkItemTracking/My Project/AssemblyInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Team Foundation WorkItem Tracking")> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c19682a1-05b4-4240-8742-3cd83a9a5236")> 
+
